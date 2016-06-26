d25bbd55fda3739871985d30a97a110118950017
kappa <kappa@kappabookair.local>
2016-06-26 09:09:13 +0900

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,15 +9,15 @@ if ENV['CIRCLECI']
     class Container
       def exec(command, opts = {}, &block)
         command[2] = command[2].inspect # ['/bin/sh', '-c', 'YOUR COMMAND']
-        cmd = %Q{sudo lxc-attach -n #{self.id} -- #{command.join(' ')}}
+        cmd = %Q{sudo lxc-attach -n #{self.id} -- bash -c #{command.join(' ')}}
         stdin, stdout, stderr, wait_thread = Open3.popen3 cmd
         [stdout.read, [stderr.read], wait_thread.value.exitstatus]
       end
-      def remove(options={})
-        # do not delete container
-      end
-      alias_method :delete, :remove
-      alias_method :kill, :remove
+      #def remove(options={})
+      #  # do not delete container
+      #end
+      #alias_method :delete, :remove
+      #alias_method :kill, :remove
     end
   end
 end
