6faaec4240c9e9e53a4228249b74e89076168117
kappa <kappa@kappabookair.local>
2016-06-26 09:35:28 +0900

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,15 @@ require 'open3'
 
 # https://circleci.com/docs/docker#docker-exec
 # https://ja.ngs.io/2015/09/26/circleci-docker-serverspec/
+
 if ENV['CIRCLECI']
   module Docker
     class Container
       def exec(command, opts = {}, &block)
         command[2] = command[2].inspect # ['/bin/sh', '-c', 'YOUR COMMAND']
+        puts command[2]
         cmd = %Q{sudo lxc-attach -n #{self.id} -- #{command.join(' ')}}
+        puts cmd
         stdin, stdout, stderr, wait_thread = Open3.popen3 cmd
         [stdout.read, [stderr.read], wait_thread.value.exitstatus]
       end
