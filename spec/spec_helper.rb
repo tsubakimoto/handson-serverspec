96fc59b2fbae3e69fb66b2f8bce6ffcbf31152e6
kappa <kappa@kappabookair.local>
2016-06-26 09:21:18 +0900

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,11 +13,11 @@ if ENV['CIRCLECI']
         stdin, stdout, stderr, wait_thread = Open3.popen3 cmd
         [stdout.read, [stderr.read], wait_thread.value.exitstatus]
       end
-      #def remove(options={})
-      #  # do not delete container
-      #end
-      #alias_method :delete, :remove
-      #alias_method :kill, :remove
+      def remove(options={})
+        # do not delete container
+      end
+      alias_method :delete, :remove
+      alias_method :kill, :remove
     end
   end
 end
