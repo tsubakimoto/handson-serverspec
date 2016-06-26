fa734e8e6a86f83480716afe43b4856ea9e5c9c5
kappa <kappa@kappabookair.local>
2016-06-26 14:52:22 +0900

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,14 +11,15 @@ if ENV['CIRCLECI']
   module Docker
     class Container
       def exec(command, opts = {}, &block)
+        puts command
         command[2] = command[2].inspect # ['/bin/sh', '-c', 'YOUR COMMAND']
         puts command[2]
         cmd = %Q{sudo lxc-attach -n #{self.id} -- #{command.join(' ')}}
         puts cmd
         stdin, stdout, stderr, wait_thread = Open3.popen3 cmd
-        p stdout.read
-        p stderr.read
-        p wait_thread.read
+        # p stdout.read
+        # p stderr.read
+        # p wait_thread.read
         [stdout.read, [stderr.read], wait_thread.value.exitstatus]
       end
       def remove(options={})
