c09dacd34f8b1ab0ce1bb809fa32d9fde86ac310
kappa <kappa@kappabookair.local>
2016-06-26 18:30:19 +0900

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ if ENV['CIRCLECI']
   module Docker
     class Container
       def exec(command, opts = {}, &block)
-        # command[2] = command[2].inspect # ['/bin/sh', '-c', 'YOUR COMMAND']
+        command[2] = command[2].inspect # ['/bin/sh', '-c', 'YOUR COMMAND']
         cmd = %Q{sudo lxc-attach -n #{self.id} -- #{command.join(' ')}}
         p cmd
         stdin, stdout, stderr, wait_thread = Open3.popen3 cmd
