4fe2f052a7a5fa0de4d742999fd79112367b47fc
kappa <kappa@kappabookair.local>
2016-06-26 18:25:31 +0900

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ if ENV['CIRCLECI']
       def exec(command, opts = {}, &block)
         # command[2] = command[2].inspect # ['/bin/sh', '-c', 'YOUR COMMAND']
         cmd = %Q{sudo lxc-attach -n #{self.id} -- #{command.join(' ')}}
+        p cmd
         stdin, stdout, stderr, wait_thread = Open3.popen3 cmd
         [stdout.read, [stderr.read], wait_thread.value.exitstatus]
       end
